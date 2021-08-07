fe952e25758d357a62348d7f0875ff7a49e4a800
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-08-07 18:10:09 +0300

    upadted _initi_.py folder

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+	app = Flask(__name__)
+
+	#create the app configurations
+	app.config.from_object(config_options[config_name])
+
+	# Initializing flask extensions
+	bootstrap.init_app(app)
+
+	#registering the blueprint
+	from .main import main as main_blueprint
+	app.register_blueprint(main_blueprint)
+
+	#setting config
+	from .requests import configure_request
+	configure_request(app)
+
+	return app
--- manage.py
+++ manage.py
@@ -1,13 +1,11 @@
 from app import create_app
 from flask_script import Manager,Server
 
-
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
-
 
 @manager.command
 def test():
@@ -15,7 +13,6 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-
 
 if __name__ == '__main__':
     manager.run()
